a8546f94e5ec1dc0bfd24452f9ed8ec3dadd1195
Nicholas McClay <nmcclay@wombatsecurity.com>
2016-10-05 09:35:55 -0400

    Commit to show how this fails to load both addons successfully

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 'use strict';
 
 module.exports = {
-  name: 'composible-test'
+  name: 'composable-helpers-font-awesome-addon-test',
+
+  included: function(app, parentAddon) {
+    // fails ember-composable-helpers
+    this._super.included(arguments);
+
+    // fails ember-font-awesome
+    // this._super.included.apply(this, arguments);
+  }
 };
